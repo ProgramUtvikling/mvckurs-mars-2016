77e66feeacdf8550ae9cfcc6548b8cd1f2852af4
Arjan Einbu <arjan@einbu.no>
2016-03-07 13:13:46 +0100

    Initial

---
+++ ImdbWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ImdbWeb
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Movies by genre route",
+                "Movie/Genre/{genrename}", new { Controller = "Movie", Action = "MoviesByGenre"});
+
+            routes.MapRoute("Person details route",
+                "Person/{id}", new { Controller = "Person", Action = "Details" }, new { id = @"\d+" });
+
+            routes.MapRoute("Cover image route",
+                "Image/{format}/{id}.jpg", new { Controller = "Image", Action = "CreateImage" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ImdbWeb/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImdbWeb.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public string Actors()
+        {
+            return "PersonController.Actors";
+        }
+        public string Producers()
+        {
+            return "PersonController.Producers";
+        }
+        public string Directors()
+        {
+            return "PersonController.Directors";
+        }
+        public string Details(int id)
+        {
+            return $"PersonController.Details({id})";
+        }
+    }
+}
